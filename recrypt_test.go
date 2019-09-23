6750f4520eb5602ce44ebcd9df89055017df5b6b
Chadwick Dahlquist <cdahlquist@lo3energy.com>
2019-09-23 16:05:55 -0700

    Initial api methods stub for exergy/PublicWiki#1

---
+++ recrypt_test.go
@@ -0,0 +1,19 @@
+package recrypt
+
+import "testing"
+
+func TestRecrypt(t *testing.T) {
+	api := NewApi256()
+
+	keys := api.generateKeyPair()
+	signingKeys := api.generateEd25519KeyPair()
+	plaintext := api.generatePlaintext()
+	encryptedValue := api.encrypt(plaintext, keys.publicKey, signingKeys.privateKey)
+	decryptedValue := api.decrypt(encryptedValue, keys.privateKey)
+
+	t.Log(keys)
+	t.Log(signingKeys)
+	t.Log(plaintext)
+	t.Log(encryptedValue)
+	t.Log(decryptedValue)
+}
